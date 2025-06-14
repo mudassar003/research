b1f91cabf48c3990c544a96d096feecce39615a1
Mudassar Rehman <mudassar.rehman2575@gmail.com>
2025-06-14 21:40:09 +0500

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from my_agents.orchestrator_agent import orchestrator_agent
 from agents import Runner
 import asyncio
 from dotenv import load_dotenv
+from agents.extensions.visualization import draw_graph
 
 load_dotenv()
 
@@ -12,6 +13,8 @@ async def main():
         input="Use the orchestrator agent and give me output that you get from it. File path for read_source_file tool is 'sample.py'.",
     )
     print(result.final_output)
+
+    draw_graph(orchestrator_agent, filename="my_workflow.png")
 
 if __name__ == "__main__":
     asyncio.run(main())
